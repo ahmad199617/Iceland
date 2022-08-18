f9116fe7c6966a424b0ac38e9d556ba7fcfccf7d
ahmad199617 <95858731+ahmad199617@users.noreply.github.com>
2022-08-18 09:23:39 +0300

    Update Tek-Katlı-Evler.js

--- Tek-Katlı-Evler.js
+++ Tek-Katlı-Evler.js
@@ -331,7 +331,8 @@ var stop = false
         Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
              document.querySelector(".M-3D-IMGX").style.opacity = "0%";
              document.querySelector(".D3D-SCALL").style.opacity = "100%";
-             start3D ()
+             lod()
+             setTimeout(start3D, 1000)
              numperx = 0
 
 
